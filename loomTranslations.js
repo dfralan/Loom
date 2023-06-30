1a766fad15cb584226800df6723d2b2b8e4544ad
dfralan <80047722+dfralan@users.noreply.github.com>
2023-06-29 22:12:11 -0300

    Create loomTranslations.js

---
+++ loomTranslations.js
@@ -0,0 +1,9 @@
+const loomTranslations = {
+    "Hello Dora": {
+      "us": "Hello Dora",
+      "ar": "Hola Dora",
+      "br": "Alô Dora",
+      "fr": "Bonjour Dora",
+      "it": "Ciao Dora"
+    },
+}
